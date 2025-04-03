6730ace97d10e1aed44c25965a779cb1df336d4e
Sandil <adhikarisandil32@gmail.com>
2025-04-04 01:57:13 +0545

    configure sqlite database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database/db.sqlite',
+      entities: [],
+      synchronize: false,
+      retryAttempts: 5,
+      retryDelay: 10 * 1000,
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
