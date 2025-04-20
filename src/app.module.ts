285617bc4f99a7f0b33be5751cb71ec9dcad4cd7
Sandil <adhikarisandil32@gmail.com>
2025-04-21 01:53:02 +0545

    app module configuration udpate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoModule } from './todo/todo.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodosModule } from './todos/todos.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'database/db.sqlite',
-      entities: [],
-      synchronize: false,
-      retryAttempts: 5,
-      retryDelay: 10 * 1000,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.NODE_ENV === 'development',
+      retryAttempts: 10,
+      retryDelay: 1000,
     }),
-    TodoModule,
+    TodosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
