4978497f6dd767dc9dbbd6c9f4c7f2bc01fdfd03
Sandil <adhikarisandil32@gmail.com>
2025-06-25 01:10:48 +0545

    fix: unnecessary imports removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
-import { Module, ValidationPipe } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodosModule } from './todos/todos.module';
 import * as dotenv from 'dotenv';
-import { APP_PIPE } from '@nestjs/core';
 
 dotenv.config();
 
@@ -14,13 +11,11 @@ dotenv.config();
       type: 'sqlite',
       database: 'database/db.sqlite',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: process.env.NODE_ENV === 'development',
+      synchronize: false,
       retryAttempts: 10,
       retryDelay: 1000,
     }),
     TodosModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, { provide: APP_PIPE, useClass: ValidationPipe }],
 })
 export class AppModule {}
