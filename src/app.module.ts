73ed89764114b3eed700e512bd7818b1138aa172
Sandil <adhikarisandil32@gmail.com>
2025-06-26 02:29:00 +0545

    feat: external datasource utilized for changest to appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,39 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodosModule } from './todos/todos.module';
+import { UsersModule } from './users/users.module';
 import * as dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
 
 dotenv.config();
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'database/db.sqlite',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false,
-      retryAttempts: 10,
-      retryDelay: 1000,
+    TypeOrmModule.forRootAsync({
+      // type: 'sqlite',
+      // database: 'database/db.sqlite',
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      // migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+      // synchronize: false,
+      // retryAttempts: 10,
+      // retryDelay: 1000,
+      useFactory: () => ({
+        type: 'sqlite',
+        database: 'database/db.sqlite',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+        synchronize: false,
+        retryAttempts: 10,
+        retryDelay: 1000,
+      }),
+      dataSourceFactory: async (options) => {
+        const datasource = await new DataSource(options).initialize();
+
+        return datasource;
+      },
     }),
     TodosModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
