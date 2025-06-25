e96e78d30aab31fce3f3b52251316bc1cdaff475
Sandil <adhikarisandil32@gmail.com>
2025-06-26 02:30:06 +0545

    feat: users resource added to app in relation to other entities

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,14 @@
+import { Todos } from 'src/todos/entities/todos.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ nullable: false, unique: true })
+  email: string;
+}
