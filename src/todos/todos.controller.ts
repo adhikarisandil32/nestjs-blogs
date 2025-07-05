1dacc0c28cf7f9d82c1e4126cf83752dab8236b9
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-06 01:52:59 +0545

    summary added to nestjs documentaiton

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -11,12 +11,14 @@ import {
 import { TodosService } from './todos.service';
 import { CreateTodosDto } from './dto/create-todos.dto';
 import { UpdateTodosDto } from './dto/update-todos.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('todos')
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
 
   // look at https://stackoverflow.com/questions/62700524/nest-js-only-accept-fields-that-are-specified-in-a-dto
+  @ApiOperation({ summary: 'Create a new todo' })
   @Post()
   // below pipe will only validate and put it into body whose dtos are created is validated
   // @UsePipes(new ValidationPipe({ whitelist: true }))
@@ -25,21 +27,25 @@ export class TodosController {
     return this.todosService.create(createTodoDto);
   }
 
+  @ApiOperation({ summary: 'List all todos' })
   @Get()
   findAll(@Query() searchParams: Record<string, any>): {} {
     return this.todosService.findAll({ searchParams });
   }
 
+  @ApiOperation({ summary: 'List a todos by id' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.todosService.findOne(+id);
   }
 
+  @ApiOperation({ summary: 'Update a todo' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTodosDto: UpdateTodosDto) {
     return this.todosService.update(+id, updateTodosDto);
   }
 
+  @ApiOperation({ summary: 'Delete a todo' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.todosService.remove(+id);
