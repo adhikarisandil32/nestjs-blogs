9a39c92cbc8c9cc4a584971a9604576609dc9363
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-06 01:52:16 +0545

    rename some variables

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,18 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
+  const swaggerDocumentBuild = new DocumentBuilder()
     .setTitle('Todos API Documentation')
     .setDescription('This is the API documentation for the todo app.')
     .setVersion('1.0')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, documentFactory);
+  const todosDocument = () =>
+    SwaggerModule.createDocument(app, swaggerDocumentBuild);
+  SwaggerModule.setup('api-docs', app, todosDocument);
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
