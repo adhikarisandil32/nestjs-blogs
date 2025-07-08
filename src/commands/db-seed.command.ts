ca1c3c6526f7f02a948e8d5119a795904c981e3b
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-09 01:15:04 +0545

    create seeder file for users

--- src/commands/db-seed.command.ts
+++ src/commands/db-seed.command.ts
@@ -1,42 +1,46 @@
 import { Command, CommandRunner } from 'nest-commander';
-import { faker } from '@faker-js/faker';
 import { MyLogger } from '../common-modules/logger.service';
 import { DataSource } from 'typeorm';
 import { AppDataSource } from '../data-source';
-import { Inject } from '@nestjs/common';
-import { plainToInstance } from 'class-transformer';
+import { faker } from '@faker-js/faker';
+import { Users } from '../users/entities/user.entity';
 
 @Command({ name: 'seed-users', description: 'A command to seed users' })
 export class SeedDatabase extends CommandRunner {
   constructor(
     private readonly _loggerService: MyLogger,
-    // @Inject(plainToInstance(AppDataSource, { ...AppDataSource }))
     private readonly _dataSource: DataSource,
   ) {
     super();
   }
 
   async run(): Promise<void> {
-    // console.log({ dataSource: this._dataSource });
     // const queryRunner = this._dataSource.createQueryRunner();
 
-    // try {
-    //   this._loggerService.log('Starting User Seed');
-    //   for (let i = 1; i <= 16; i++) {
-    //     queryRunner.manager.insert(Users, {
-    //       name: faker.internet.displayName(),
-    //       email: faker.internet.email(),
-    //       password: 'Test@123',
-    //     });
-    //     // .create({
-    //     //   email: faker.internet.email(),
-    //     // });
-    //   }
-    //   this._loggerService.log('User Seeding Successful');
-    // } catch (error) {
-    //   this._loggerService.error(error);
-    // }
-
-    this._loggerService.log('hello');
+    try {
+      // await queryRunner.connect();
+      this._loggerService.log('Starting User Seed');
+      for (let i = 1; i <= 16; i++) {
+        // await queryRunner.manager.insert(Users, {
+        //   name: faker.internet.displayName(),
+        //   email: faker.internet.email(),
+        //   password: 'Test@123',
+        // });
+        // console.log(`User ${i} inserted`);
+        // .create({
+        //   email: faker.internet.email(),
+        // });
+        await this._dataSource.manager.insert(Users, {
+          name: faker.person.fullName(),
+          email: faker.internet.email(),
+          password: 'Test@123',
+        });
+        console.log(`User ${i} inserted`);
+      }
+      this._loggerService.log('User Seeding Successful');
+      // await queryRunner.release();
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
