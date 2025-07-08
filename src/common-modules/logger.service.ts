a03e04a6cdda1f5d123236b8f939c8d724ddd80d
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-08 15:03:38 +0545

    update logger service to extend ConsoleLogger

--- src/common-modules/logger.service.ts
+++ src/common-modules/logger.service.ts
@@ -1,5 +1,31 @@
-export class MyLogger {
-  log(message: any) {
-    console.log(message);
+import { ConsoleLogger } from '@nestjs/common';
+
+export class MyLogger extends ConsoleLogger {
+  log(message: any, context?: string) {
+    if (
+      context !== 'RouterExplorer' &&
+      context !== 'NestApplication' &&
+      context !== 'InstanceLoader' &&
+      context !== 'RoutesResolver' &&
+      context !== 'NestFactory'
+    ) {
+      super.log(message, context);
+    }
+  }
+
+  error(message: any, trace?: string, context?: string) {
+    super.error(message, trace, context);
+  }
+
+  warn(message: any, context?: string) {
+    super.warn(message, context);
+  }
+
+  debug(message: any, context?: string) {
+    super.debug(message, context);
+  }
+
+  verbose(message: any, context?: string) {
+    super.verbose(message, context);
   }
 }
