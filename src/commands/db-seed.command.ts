b514e8473f61bbabbccd9449941ea4ba61274a82
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-09 01:29:45 +0545

    add comment on why use typeorm's configuration in cli.module.ts as well

--- src/commands/db-seed.command.ts
+++ src/commands/db-seed.command.ts
@@ -1,35 +1,27 @@
 import { Command, CommandRunner } from 'nest-commander';
 import { MyLogger } from '../common-modules/logger.service';
 import { DataSource } from 'typeorm';
-import { AppDataSource } from '../data-source';
 import { faker } from '@faker-js/faker';
 import { Users } from '../users/entities/user.entity';
+// import { AppDataSource } from '../data-source';
 
 @Command({ name: 'seed-users', description: 'A command to seed users' })
 export class SeedDatabase extends CommandRunner {
+  // private readonly _dataSource: DataSource
   constructor(
     private readonly _loggerService: MyLogger,
     private readonly _dataSource: DataSource,
   ) {
     super();
+    // this._dataSource = AppDataSource
+
+    // no such hassle, because we've used the same module configuration for typeorm in main.ts and cli.module.ts, we can directly inject data source here as well
   }
 
   async run(): Promise<void> {
-    // const queryRunner = this._dataSource.createQueryRunner();
-
     try {
-      // await queryRunner.connect();
       this._loggerService.log('Starting User Seed');
       for (let i = 1; i <= 16; i++) {
-        // await queryRunner.manager.insert(Users, {
-        //   name: faker.internet.displayName(),
-        //   email: faker.internet.email(),
-        //   password: 'Test@123',
-        // });
-        // console.log(`User ${i} inserted`);
-        // .create({
-        //   email: faker.internet.email(),
-        // });
         await this._dataSource.manager.insert(Users, {
           name: faker.person.fullName(),
           email: faker.internet.email(),
@@ -38,7 +30,6 @@ export class SeedDatabase extends CommandRunner {
         console.log(`User ${i} inserted`);
       }
       this._loggerService.log('User Seeding Successful');
-      // await queryRunner.release();
     } catch (error) {
       console.error(error);
     }
