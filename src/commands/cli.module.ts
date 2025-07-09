32ca9bdc49d393c68f19e1604b86c5d85c643199
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-09 11:14:21 +0545

    remove dotenv config form cli module as database module handles it

--- src/commands/cli.module.ts
+++ src/commands/cli.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MyLogger } from '../common-modules/logger.service';
-import * as dotenv from 'dotenv';
 import { DatabaseModule } from '../common-modules/database/database.module';
 import { SeedUsersDatabase } from './db-seed.command';
-
-dotenv.config();
 
 // console.log(__dirname)
 @Module({
