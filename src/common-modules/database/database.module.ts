67fd8c6991b570445576377e1bcbf9b9f799ee09
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-09 11:10:59 +0545

    create database module seperately that will contain config of the database to be used for cli as well as app bootstrap

---
+++ src/common-modules/database/database.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        host: 'localhost',
+        port: +process.env.DATABASE_PORT,
+        entities: [__dirname + './../../**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+        synchronize: false,
+      }),
+      dataSourceFactory: async (options) => {
+        const datasource = await new DataSource(options).initialize();
+
+        return datasource;
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
