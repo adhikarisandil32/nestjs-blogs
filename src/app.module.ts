5ebf42e631921966a26bb202a73a81edf80d7aaa
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-09 11:11:22 +0545

    use same database.module for app bootstrap and cli.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TodosModule } from './todos/todos.module';
 import { UsersModule } from './users/users.module';
-import * as dotenv from 'dotenv';
-import { DataSource } from 'typeorm';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoggerModule } from 'nestjs-pino';
-
-dotenv.config();
+import { DatabaseModule } from './common-modules/database/database.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        host: 'localhost',
-        port: Number(process.env.DATABASE_PORT),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-        synchronize: false,
-      }),
-      dataSourceFactory: async (options) => {
-        const datasource = await new DataSource(options).initialize();
-
-        return datasource;
-      },
-    }),
+    DatabaseModule,
     LoggerModule.forRoot({
       pinoHttp: {
         transport: {
--- src/commands/cli.module.ts
+++ src/commands/cli.module.ts
@@ -1,33 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SeedDatabase } from './db-seed.command';
 import { MyLogger } from '../common-modules/logger.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
+import { DatabaseModule } from '../common-modules/database/database.module';
 
 dotenv.config();
 
 // console.log(__dirname)
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        host: 'localhost',
-        port: Number(process.env.DATABASE_PORT),
-        entities: [__dirname + './../**/*.entity{.ts,.js}'], // console.log(__dirname) to see where it is looking entities from
-        synchronize: false,
-      }),
-      dataSourceFactory: async (options) => {
-        const datasource = await new DataSource(options).initialize();
-
-        return datasource;
-      },
-    }),
-  ],
+  imports: [DatabaseModule],
   providers: [SeedDatabase, MyLogger],
 })
 export class CliModule {}
