d81f02ed82b651286e3255326759d3088902bd2a
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-11 14:54:36 +0545

    add migrations

--- src/migrations/1752213315518-init.ts
+++ src/migrations/1752213315518-init.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Init1751607094546 implements MigrationInterface {
-    name = 'Init1751607094546'
+export class Init1752213315518 implements MigrationInterface {
+    name = 'Init1752213315518'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP WITH TIME ZONE, "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
