50ee5ee6101456f07142d322c0fdcc49ccb0dfc0
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-14 01:19:16 +0545

    implement jwt in all packages

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,17 @@
 import { Controller, Post, Body, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { authDto } from './dto/auth.dto';
-import { ApiBody } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('/login')
+  @Post('login')
   login(@Body() authDto: authDto) {
     return this.authService.login(authDto);
   }
 
-  @Get('/me')
+  @Get('me')
   me() {
     return this.authService.findOne();
   }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,34 +1,49 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { Users } from '../users/entities/user.entity';
 import { authDto } from './dto/auth.dto';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly _dataSource: DataSource) {}
+  constructor(
+    private readonly _dataSource: DataSource,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async login(authDto: authDto) {
     const existingUser = await this._dataSource.manager.findOne(Users, {
       where: {
         email: authDto.email,
       },
-      select: {
-        createdAt: true,
-        updatedAt: true,
-        deletedAt: true,
-        email: true,
-        id: true,
-        name: true,
-      },
     });
 
     if (!existingUser) throw new NotFoundException("User Doesn't Exist");
+
+    const isPasswordVerified = await bcrypt.compare(
+      authDto.password,
+      existingUser.password,
+    );
+
+    if (!isPasswordVerified)
+      throw new UnauthorizedException("email or password does't match");
+
+    const { password, ...otherData } = existingUser;
+    const accessToken = this.jwtService.sign(
+      { id: existingUser.id, email: existingUser.email },
+      { secret: process.env.JWT_SECRET, expiresIn: process.env.JWT_EXPIRES_IN },
+    );
 
     return {
       message: 'User Login success',
       status: 200,
       success: true,
-      data: existingUser,
+      data: { ...otherData, accessToken },
     };
   }
 
