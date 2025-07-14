47f6b21de2b3d58f770e001d6c93e97743029aec
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-15 00:49:19 +0545

    add comment about binding interceptors on how to use interceptors globally

--- src/common-modules/response/respose.module.ts
+++ src/common-modules/response/respose.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ResponseInterceptor } from './response.interceptor';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 
+// READ `Binding Interceptors` for using it globally from here: https://docs.nestjs.com/interceptors#binding-interceptors
 @Module({
   controllers: [],
   providers: [
