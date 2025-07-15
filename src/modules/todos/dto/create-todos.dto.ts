525d7368899e25a05ddbe77d0ab323f2c2af68bc
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-15 23:58:27 +0545

    add ApiProperty to create todos dto

--- src/modules/todos/dto/create-todos.dto.ts
+++ src/modules/todos/dto/create-todos.dto.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTodosDto {
+  @ApiProperty({ example: 'My Todo Title' })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({ example: 'This is the description of my todo' })
   @IsString()
   description: string;
 
+  @ApiProperty({ example: false })
   @IsBoolean()
   isCompleted: boolean;
 }
