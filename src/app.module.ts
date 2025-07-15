809735fc125149a80b551568ee50daf9ef19d7dd
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-15 14:08:44 +0545

    refactor to seperate imports from common modules and actual database modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,9 @@
 import { Module } from '@nestjs/common';
-import { LoggerModule } from 'nestjs-pino';
-import { DatabaseModule } from './common-modules/database/database.module';
-import { AuthModule } from './auth/auth.module';
 import { TodosModule } from './modules/todos/todos.module';
 import { UsersModule } from './modules/users/users.module';
-import { ResponseModule } from './common-modules/response/respose.module';
+import { CommonModules } from './common-modules/common.module';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    LoggerModule.forRoot({
-      pinoHttp: {
-        transport: {
-          target: 'pino-pretty',
-        },
-      },
-    }),
-    ResponseModule,
-    AuthModule,
-    TodosModule,
-    UsersModule,
-  ],
+  imports: [CommonModules, TodosModule, UsersModule],
 })
 export class AppModule {}
