edf0d32011b5eee19f8efdba1f62e089f858d5da
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-16 18:11:23 +0545

    add command module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { TodosModule } from './modules/todos/todos.module';
 import { UsersModule } from './modules/users/users.module';
 import { CommonModules } from './common-modules/common.module';
+import { AdminsModule } from './modules/admins/admins.module';
+import { RolesModule } from './modules/roles/roles.module';
+import { CommandModule } from 'nestjs-command';
 
 @Module({
-  imports: [CommonModules, TodosModule, UsersModule],
+  imports: [
+    CommandModule,
+    CommonModules,
+    TodosModule,
+    UsersModule,
+    AdminsModule,
+    RolesModule,
+  ],
 })
 export class AppModule {}
