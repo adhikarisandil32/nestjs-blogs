e3221ffbf5135bad570e6968c1d71254fb1c93eb
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-16 18:17:31 +0545

    cli.module setup with nestjs-command

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,34 @@
-import { CommandFactory } from 'nest-commander';
-import { CliModule } from './commands/cli.module';
+// previously installed command module
+
+// import { CommandFactory } from 'nest-commander';
+// import { CliModule } from './commands/cli.module';
+
+// async function bootstrap() {
+//   // await CommandFactory.run(AppModule);
+
+//   // or, if you only want to print Nest's warnings and errors
+//   await CommandFactory.run(CliModule, ['warn', 'error']);
+// }
+
+// bootstrap();
+
+import { NestFactory } from '@nestjs/core';
+import { CommandModule, CommandService } from 'nestjs-command';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // await CommandFactory.run(AppModule);
+  const app = await NestFactory.createApplicationContext(AppModule, {
+    logger: false,
+  });
 
-  // or, if you only want to print Nest's warnings and errors
-  await CommandFactory.run(CliModule, ['warn', 'error']);
+  try {
+    await app.select(CommandModule).get(CommandService).exec();
+    await app.close();
+  } catch (error) {
+    console.error(error);
+    await app.close();
+    process.exit(1);
+  }
 }
 
 bootstrap();
