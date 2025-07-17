0e12a7435ad8462b846bc11b3b8e57b16e836c1e
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-17 18:05:48 +0545

    cli use nestjs-command instead of nest-commander

--- src/cli.ts
+++ src/cli.ts
@@ -14,21 +14,20 @@
 
 import { NestFactory } from '@nestjs/core';
 import { CommandModule, CommandService } from 'nestjs-command';
-import { AppModule } from './app.module';
+import { CliModule } from './commands/cli.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule, {
-    logger: false,
+  const app = await NestFactory.createApplicationContext(CliModule, {
+    logger: ['error', 'warn'],
   });
 
   try {
     await app.select(CommandModule).get(CommandService).exec();
-    await app.close();
   } catch (error) {
-    console.error(error);
-    await app.close();
-    process.exit(1);
+    console.log(error);
+    app.close();
   }
+  process.exit(0);
 }
 
 bootstrap();
