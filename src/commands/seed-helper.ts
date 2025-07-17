9fe9ea4d160f97b8fd87a6366c7c3c920f04ec5e
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-17 18:07:17 +0545

    write seed helper to seed data to database

--- src/commands/seed-helper.ts
+++ src/commands/seed-helper.ts
@@ -1,21 +1,39 @@
 import { UserRole } from 'src/constants/user-roles.constant';
-import { Role } from 'src/modules/roles/entities/role.entity';
+import { Roles } from 'src/modules/roles/entities/role.entity';
 import { QueryRunner } from 'typeorm';
 
 export async function seedUserRoles(queryRunner: QueryRunner) {
-  const existingRoles = await queryRunner.manager.find(Role);
+  console.log('seeding roles');
+  const existingRoles = await queryRunner.manager.find(Roles);
 
-  const seedingRequiredForRoles = Object.values(UserRole).filter(
+  const seedingRequiredForRoles: UserRole[] = Object.values(UserRole).filter(
     (roleNameFromApp) => {
-      return existingRoles.find(
+      return !existingRoles.find(
         (roleNameFromDB) => roleNameFromDB.role !== roleNameFromApp,
       );
     },
   );
 
-  console.log(seedingRequiredForRoles);
+  if (seedingRequiredForRoles.length <= 0) {
+    console.log('roles not defined or defined roles already exists');
+    return;
+  }
 
-  // queryRunner.manager.create(Role, {
-  //   role: UserRole.ADMIN,
-  // });
+  // console.log(
+  //   seedingRequiredForRoles.map((role) => ({
+  //     role,
+  //   })),
+  // );
+
+  const seedReadyRoles = queryRunner.manager.create(
+    Roles,
+    seedingRequiredForRoles.map((role) => ({
+      role,
+    })),
+  );
+
+  await queryRunner.manager.save(seedReadyRoles);
+
+  console.log('seeding roles complete');
+  return;
 }
