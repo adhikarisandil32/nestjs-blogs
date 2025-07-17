61d2db927ce3053651619892798decc9c6e04670
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-18 02:30:19 +0545

    seperate user seed logic to seed-helper.ts file

--- src/commands/user-seed.command.ts
+++ src/commands/user-seed.command.ts
@@ -1,16 +1,15 @@
-import { MyLogger } from 'src/common-modules/logger.service';
 import { DataSource } from 'typeorm';
 import { Command } from 'nestjs-command';
 import { Injectable } from '@nestjs/common';
-import { seedUserRoles } from './seed-helper';
-// import { AppDataSource } from '../data-source';
+import { seedUserRoles, seedUsers } from './seed-helper';
+import { MyLogger } from 'src/common-modules/logger.service';
 
 @Injectable()
 export class SeedUsersDatabase {
   // private readonly _dataSource: DataSource
   constructor(
-    private readonly _loggerService: MyLogger,
     private readonly _dataSource: DataSource,
+    private readonly _loggerService: MyLogger,
   ) {
     // this._dataSource = AppDataSource
     // no such hassle, because we've used the same module configuration for typeorm in main.ts and cli.module.ts, we can directly inject data source here as well
@@ -21,19 +20,8 @@ export class SeedUsersDatabase {
     const queryRunner = this._dataSource.createQueryRunner();
 
     try {
-      this._loggerService.log('Seeding Start');
-      await seedUserRoles(queryRunner);
-      // for (let i = 1; i <= 16; i++) {
-      //   const user = queryRunner.manager.create(Users, {
-      //     name: faker.person.fullName(),
-      //     email: faker.internet.email().toLowerCase(),
-      //     password: 'Test@123',
-      //   });
-      //   await queryRunner.manager.save(user);
-      //   // using save is important because user entity uses @BeforeInsert decorator which will only act when save used
-      //   console.log(`User ${i} inserted`);
-      // }
-      this._loggerService.log('Seeding Successful');
+      await seedUserRoles(queryRunner, this._loggerService);
+      await seedUsers(queryRunner, this._loggerService);
     } catch (error) {
       console.error(error);
     }
