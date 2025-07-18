10a44a7c456a3a03ac56cbd2fec9069abc10b651
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-18 14:32:30 +0545

    provide type to todosJson import and also error context and stack

--- src/commands/todo-seed.command.ts
+++ src/commands/todo-seed.command.ts
@@ -3,6 +3,7 @@ import { DataSource } from 'typeorm';
 import { Todos } from 'src/modules/todos/entities/todos.entity';
 import { Command } from 'nestjs-command';
 import { Injectable } from '@nestjs/common';
+import { Users } from 'src/modules/users/entities/user.entity';
 
 @Injectable()
 export class SeedTodoDatabase {
@@ -11,13 +12,24 @@ export class SeedTodoDatabase {
     private readonly _dataSource: DataSource,
   ) {}
 
+  /****************
+   * Todos Seeder *
+   ****************/
   @Command({ command: 'seed-todos', describe: 'A command to seed todos' })
   async run(): Promise<void> {
-    const todosJson = require('./todos-json/todos.json');
+    const todosJson: {
+      title: string;
+      description: string;
+    }[] = require('./todos-json/todos.json');
+    const SeedTodosContext = 'todos-seed';
 
     const queryRunner = this._dataSource.createQueryRunner();
 
-    const SeedTodosContext = 'todos-seed';
+    const users = await queryRunner.manager.find(Users);
+
+    if (!users) {
+      throw new Error('No exisitng users to create relation');
+    }
 
     try {
       this._loggerService.log('Starting Todos Seed', SeedTodosContext);
@@ -25,7 +37,7 @@ export class SeedTodoDatabase {
         const todo = queryRunner.manager.create(Todos, {
           ...todosJson[i],
           isCompleted: Boolean(Math.round(Math.random() * 2)),
-          user: Math.ceil(Math.random() * 16),
+          user: users[Math.ceil(Math.random() * users.length)],
         });
         await queryRunner.manager.save(todo);
         console.log(`Todo ${i} inserted`);
