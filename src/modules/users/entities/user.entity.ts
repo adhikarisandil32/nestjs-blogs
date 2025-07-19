2ef1cf4c77fe6d7e65e57bbd16b258d6c6bc30a5
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-19 18:25:31 +0545

    user base entity created for user and admin

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,27 +1,10 @@
-import { DBBaseEntity } from 'src/common-modules/database/base.entity';
-import { BeforeInsert, Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
-import * as bcrypt from 'bcryptjs';
+import { Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { Roles } from 'src/modules/roles/entities/role.entity';
+import { UsersBaseEntity } from 'src/common-modules/entities/user-base.entity';
 
 @Entity()
-export class Users extends DBBaseEntity {
-  @Column({ nullable: false })
-  name: string;
-
-  @Column({ nullable: false, unique: true })
-  email: string;
-
-  @Column({ nullable: false })
-  password: string;
-
+export class Users extends UsersBaseEntity {
   @ManyToOne(() => Roles, (role) => role.id, { nullable: false })
   @JoinColumn({ name: 'role_id' })
   role: Roles;
-
-  @BeforeInsert()
-  async hashPassword() {
-    if (this.password) {
-      this.password = await bcrypt.hash(this.password, 10);
-    }
-  }
 }
