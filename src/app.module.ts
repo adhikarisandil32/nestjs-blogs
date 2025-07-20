3ce6a1154d8c2e994a35ab396192a04de68ac904
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-21 02:16:27 +0545

    import fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommonModules } from './common-modules/common.module';
-import { RouterModule } from './router/router.module';
+import { AdminsModule } from './modules/admins/admins.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { TodosModule } from './modules/todos/todos.module';
 
 @Module({
-  imports: [CommonModules, RouterModule],
+  imports: [CommonModules, AuthModule, AdminsModule, UsersModule, TodosModule],
 })
 export class AppModule {}
