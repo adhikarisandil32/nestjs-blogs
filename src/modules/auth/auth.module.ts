fb862a72152777f64dc5af516e002e7284cb0f00
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-21 02:16:05 +0545

    todos-import controller fixes

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
 import { JwtService } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
+import { AuthControllerPublic } from './controllers/public.auth.controller';
+import { AuthControllerAdmin } from './controllers/admin.auth.controller';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AuthController],
+  controllers: [AuthControllerAdmin, AuthControllerPublic],
   providers: [AuthService, JwtService],
 })
 export class AuthModule {}
--- src/modules/todos/todos.module.ts
+++ src/modules/todos/todos.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TodosService } from './todos.service';
-import { TodosController } from './todos.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Todos } from './entities/todos.entity';
 import { JwtService } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
+import { TodosControllerPublic } from './controllers/public.todos.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Todos]), UsersModule],
-  controllers: [TodosController],
+  controllers: [TodosControllerPublic],
   providers: [TodosService, JwtService],
 })
 export class TodosModule {}
