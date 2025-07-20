4693b67a6c8aea1ce4131c07bb7975ee09329306
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-21 02:15:01 +0545

    router setup reset for now

--- src/router/routes/admin.route.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from 'src/modules/auth/auth.module';
-import { TodosModule } from 'src/modules/todos/todos.module';
-import { UsersModule } from 'src/modules/users/users.module';
-
-@Module({
-  imports: [AuthModule, TodosModule, UsersModule],
-})
-export class AdminRouterModule {}
--- src/router/routes/public.route.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from 'src/modules/auth/auth.module';
-import { TodosModule } from 'src/modules/todos/todos.module';
-
-@Module({
-  imports: [AuthModule, TodosModule],
-})
-export class PublicRouterModule {}
