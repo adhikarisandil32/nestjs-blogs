c30ca1d22a404d44a214d7a3a25806ebee25ac0d
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-20 19:19:58 +0545

    rename the join column in admins table to role_id instead of roleId which is by default

--- src/modules/admins/entities/admin.entity.ts
+++ src/modules/admins/entities/admin.entity.ts
@@ -1,9 +1,10 @@
 import { UsersBaseEntity } from 'src/common-modules/entities/user-base.entity';
 import { Roles } from 'src/modules/roles/entities/role.entity';
-import { Entity, ManyToOne } from 'typeorm';
+import { Entity, JoinColumn, ManyToOne } from 'typeorm';
 
 @Entity()
 export class Admins extends UsersBaseEntity {
   @ManyToOne(() => Roles, (role) => role.id, { nullable: false })
+  @JoinColumn({ name: 'role_id' })
   role: Roles;
 }
