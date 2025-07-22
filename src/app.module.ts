6095009aa00baac7b0bae6c680e90b5c3b9daf8c
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-22 15:55:51 +0545

    remove other unnecessary and unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CommonModules } from './common-modules/common.module';
-import { AdminsModule } from './modules/admins/admins.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { UsersModule } from './modules/users/users.module';
-import { TodosModule } from './modules/todos/todos.module';
+import { RouterModule } from './router/router.module';
 
 @Module({
-  imports: [CommonModules, AuthModule, AdminsModule, UsersModule, TodosModule],
+  imports: [CommonModules, RouterModule],
 })
 export class AppModule {}
