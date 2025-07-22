5af7185cc35440f703cd434a6e768346ee34f243
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-22 15:56:55 +0545

    create router module and seperate admin and public users route

---
+++ src/router/router.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { RouterModule as NestRouter } from '@nestjs/core';
+import { AdminRouteModules } from './routes/admin.routes.module';
+import { PublicRouteModules } from './routes/public.routes.module';
+
+@Module({
+  imports: [
+    AdminRouteModules,
+    PublicRouteModules,
+    NestRouter.register([
+      {
+        path: 'api/admin',
+        module: AdminRouteModules,
+      },
+      {
+        path: 'api/public',
+        module: PublicRouteModules,
+      },
+    ]),
+  ],
+})
+export class RouterModule {}
