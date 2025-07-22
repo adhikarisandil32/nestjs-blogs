6b3ab53a9080ca5034865e80fed70619c5d2a6a2
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-22 15:59:30 +0545

    remove api prefix as it is done by router setup alone

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { swaggerInit } from './swagger';
-import { API_PREFIX } from './constants/controller-prefix.constant';
+import { swaggerInit } from './swagger-nest-router';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestApplication>(AppModule);
 
   // set global prefix before swagger initialization
-  if (API_PREFIX) {
-    app.setGlobalPrefix(API_PREFIX);
-  }
+  // not necessary if nest router is utilized
+  // if (API_PREFIX) {
+  //   app.setGlobalPrefix(API_PREFIX);
+  // }
 
   /* Swagger Setup moved to swagger.ts file */
   await swaggerInit(app);
