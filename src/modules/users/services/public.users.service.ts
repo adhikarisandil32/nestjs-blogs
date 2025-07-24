176e161ffa8357abdf356b79f4bb38ab982eb05d
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-24 18:07:10 +0545

    write public user create logic and findALl logic with relations

--- src/modules/users/services/public.users.service.ts
+++ src/modules/users/services/public.users.service.ts
@@ -1,25 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from '../entities/user.entity';
+import { Roles } from 'src/modules/roles/entities/role.entity';
+import { UserRole } from 'src/constants/user-roles.constant';
+import { Admins } from 'src/modules/admins/entities/admin.entity';
 
 @Injectable()
 export class UsersServicePublic {
   constructor(
     @InjectRepository(Users)
     private usersRepository: Repository<Users>,
+    private readonly _dataSource: DataSource,
   ) {}
 
   async create(createUserDto: CreateUserDto) {
-    const user = this.usersRepository.create(createUserDto);
+    const existingUserInUsers = await this.usersRepository.findOne({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+    if (existingUserInUsers)
+      throw new ConflictException('user already exists with this email');
+
+    const existingUsersInAdmins = await this._dataSource.manager.findOne(
+      Admins,
+      {
+        where: {
+          email: createUserDto.email,
+        },
+      },
+    );
+    if (existingUsersInAdmins)
+      throw new ConflictException('user already exists with this email');
+
+    const preparedData = await this.prepareUserCreateData(createUserDto);
+
+    const user = this.usersRepository.create(preparedData);
 
     await this.usersRepository.save(user);
 
-    const { password, ...others } = user;
+    delete user.password;
 
-    return others;
+    return user;
   }
 
   async findAll() {
@@ -35,6 +60,9 @@ export class UsersServicePublic {
           id: true,
           role: true,
         },
+      },
+      relations: {
+        role: true,
       },
     });
 
@@ -72,5 +100,22 @@ export class UsersServicePublic {
 
   remove() {
     return `This action removes me`;
+  }
+
+  async prepareUserCreateData(createUserDto: CreateUserDto) {
+    const publicUserRole = await this._dataSource.manager.findOne(Roles, {
+      where: {
+        role: UserRole.USER,
+      },
+      select: {
+        id: true,
+        role: true,
+      },
+    });
+
+    return {
+      ...createUserDto,
+      role: publicUserRole,
+    };
   }
 }
