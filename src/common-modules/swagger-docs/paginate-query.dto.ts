4ea8eff8f06b72ca894167189db04d5c7825f413
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-25 17:50:24 +0545

    add more fields to paginate query

--- src/common-modules/swagger-docs/paginate-query.dto.ts
+++ src/common-modules/swagger-docs/paginate-query.dto.ts
@@ -1,29 +1,49 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNumber, IsOptional, IsString, Max, Min } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class PaginatedQueryDto {
   @ApiPropertyOptional({
-    name: 'sort',
     type: 'string',
-    description: 'Enter in the format of property_key.sort_order',
-    example: 'createdAt.asc',
+  })
+  @IsOptional()
+  @IsString()
+  searchField: string;
+
+  @ApiPropertyOptional({
+    type: 'string',
+  })
+  @IsOptional()
+  @IsString()
+  search: string;
+
+  @ApiPropertyOptional({
+    type: 'string',
+    example: 'createdAt',
   })
   @IsString()
   @IsOptional()
-  sort: string;
+  sortField: string;
 
   @ApiPropertyOptional({
-    name: 'limit',
+    enum: ['ASC', 'DESC'],
+  })
+  @IsOptional()
+  @IsString()
+  sortOrder: string;
+
+  @ApiPropertyOptional({
     type: 'integer',
+    description: 'between 1 and 100',
   })
   @IsNumber()
   @IsOptional()
+  @Min(1)
+  @Max(100)
   @Type(() => Number)
   limit: number;
 
   @ApiPropertyOptional({
-    name: 'page',
     type: 'integer',
   })
   @IsNumber()
