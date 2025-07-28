52f701ec85ff718f1b7f405b93836ce0dd776bee
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-29 01:56:55 +0545

    convert plain to instance request.query inside resposne.interceptor to get serizlied data

--- src/common-modules/response/response.interceptor.ts
+++ src/common-modules/response/response.interceptor.ts
@@ -9,6 +9,8 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Request, Response } from 'express';
 import { IPaginationMetadata } from './interfaces/pagination-metadata.interface';
+import { PaginatedQueryDto } from '../swagger-docs/paginate-query.dto';
+import { plainToInstance } from 'class-transformer';
 
 export class ResponseInterceptor implements NestInterceptor {
   constructor(@Inject() private readonly _reflector: Reflector) {}
@@ -22,6 +24,8 @@ export class ResponseInterceptor implements NestInterceptor {
         const ctx = context.switchToHttp();
         const response: Response = ctx.getResponse();
         const request = ctx.getRequest<Request>();
+
+        const requestQuery = plainToInstance(PaginatedQueryDto, request.query);
 
         const message = this._reflector.get('message', context.getHandler());
         const showPagination = this._reflector.get(
@@ -45,20 +49,36 @@ export class ResponseInterceptor implements NestInterceptor {
           };
         }
 
-        const _paginationMetadata: IPaginationMetadata = {
-          total: count,
-          limit: Number(request.query?.limit ?? 10),
-          page: Number(request.query?.page ?? 1),
-          get totalPage() {
-            return Math.ceil(count / this.limit);
-          },
-          get nextPage() {
-            return this.page >= this.totalPage ? null : this.page + 1;
-          },
-          get prevPage() {
-            return this.page <= 1 ? null : this.page - 1;
-          },
-        };
+        let _paginationMetadata: IPaginationMetadata;
+
+        // console.log({ queryParams: requestQuery.skipPagination });
+
+        if (requestQuery?.skipPagination) {
+          _paginationMetadata = {
+            total: count,
+            limit: 0,
+            page: 0,
+            totalPage: 0,
+            prevPage: null,
+            nextPage: null,
+            skipPagination: true,
+          };
+        } else {
+          _paginationMetadata = {
+            total: count,
+            limit: requestQuery.limit ?? 10,
+            page: requestQuery.page ?? 1,
+            get totalPage() {
+              return Math.ceil(count / this.limit);
+            },
+            get prevPage() {
+              return this.page <= 1 ? null : this.page - 1;
+            },
+            get nextPage() {
+              return this.page >= this.totalPage ? null : this.page + 1;
+            },
+          };
+        }
 
         return {
           message: message ?? 'Request Success',
