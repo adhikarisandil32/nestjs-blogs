444c50928b47a2ac36718afc33c05cff4323e4f0
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-29 10:25:23 +0545

    remove roles controller from roles module as it creates duplicate entires on swagger

--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
-import { RolesController } from './controllers/admin.roles.controller';
 
 @Module({
-  controllers: [RolesController],
   providers: [RolesService],
   exports: [RolesService],
 })
