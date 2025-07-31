349b1d5c46d5e65b5885054a43afc17148c282f7
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-01 02:02:14 +0545

    userUpdateDto doesn't extend partial createUserDto

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,16 +1,15 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  // @ApiProperty({
-  //   type: 'string',
-  //   example: '',
-  // })
-  // @IsString()
-  // @IsNotEmpty()
-  // name: string;
+export class UpdateUserDto {
+  @ApiProperty({
+    type: 'string',
+    example: '',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  name: string;
 }
 
 export class UpdateUserPassword {
