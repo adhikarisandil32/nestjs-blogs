90ca5cecd68920b5f39fe33674f1a7b21c8e899b
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-31 18:13:20 +0545

    crate UpdateUserPassword dto

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,4 +1,33 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  // @ApiProperty({
+  //   type: 'string',
+  //   example: '',
+  // })
+  // @IsString()
+  // @IsNotEmpty()
+  // name: string;
+}
+
+export class UpdateUserPassword {
+  @ApiProperty({
+    type: 'string',
+    example: '',
+  })
+  @IsString()
+  @IsNotEmpty()
+  oldPassword: string;
+
+  @ApiProperty({
+    type: 'string',
+    example: '',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  newPassword: string;
+}
