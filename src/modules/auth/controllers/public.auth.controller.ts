f6570fc7b26d9a3c148963ac1c0663d363f81376
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-01 11:07:46 +0545

    provide change-password controller to auth module

--- src/modules/auth/controllers/public.auth.controller.ts
+++ src/modules/auth/controllers/public.auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, Patch } from '@nestjs/common';
 import { authDto } from '../dto/auth.dto';
 import { ResponseMessage } from 'src/common-modules/response/decorators/response.decorator';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
@@ -6,6 +6,7 @@ import { PutPublicUser } from '../decorator/put-user.decorator';
 import { AuthServicePublic } from '../services/public.auth.service';
 import { Users } from 'src/modules/users/entities/user.entity';
 import { User } from 'src/common-modules/request/decorators/request.decorator';
+import { UpdateUserPasswordDto } from 'src/modules/users/dto/update-user.dto';
 
 @ApiTags('Authentication')
 // @Controller(`${ControllerPrefix.PUBLIC}/auth`)
@@ -21,10 +22,18 @@ export class AuthControllerPublic {
   }
 
   @ResponseMessage('logged in user detail fetch success')
-  @ApiBearerAuth()
   @PutPublicUser()
   @Get('me')
   me(@User() user: Users) {
     return this.authService.findMe(user);
+  }
+
+  @PutPublicUser()
+  @Patch('change-password')
+  changePassword(
+    @User() user: Users,
+    @Body() passwords: UpdateUserPasswordDto,
+  ) {
+    return this.authService.changePassword({ user, passwords });
   }
 }
