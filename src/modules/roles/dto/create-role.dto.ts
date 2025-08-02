a75d198b2e0dd043022c2e6e7a44e1decf4b3fe2
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-02 16:50:49 +0545

    create create-role.dto

--- src/modules/roles/dto/create-role.dto.ts
+++ src/modules/roles/dto/create-role.dto.ts
@@ -1,1 +1,12 @@
-export class CreateRoleDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateRoleDto {
+  @ApiProperty({
+    type: 'string',
+    example: '',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
