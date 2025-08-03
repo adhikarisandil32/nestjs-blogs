20fed4f1eecaf7de529079e7cf5e5c66a4d6cc1e
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-04 00:17:41 +0545

    extend DBBaseEntity to create create-roles.dto

--- src/modules/roles/dto/create-role.dto.ts
+++ src/modules/roles/dto/create-role.dto.ts
@@ -1,7 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
+import { DBBaseEntity } from 'src/common-modules/entities/base.entity';
 
-export class CreateRoleDto {
+export class CreateRoleDto extends DBBaseEntity {
   @ApiProperty({
     type: 'string',
     example: '',
