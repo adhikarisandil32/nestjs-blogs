18c5ea2604c1a3cd9e50d0667cc481d92c74cd36
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-04 00:16:17 +0545

    add rolesRepository to import on roles.module

--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Roles } from './entities/role.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Roles])],
   providers: [RolesService],
   exports: [RolesService],
 })
