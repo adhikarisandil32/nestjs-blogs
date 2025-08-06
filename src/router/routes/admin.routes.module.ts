38f98cb7446a0af50080014f3161bf39d7556862
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-06 18:00:57 +0545

    add posts module to admin and public routes

--- src/router/routes/admin.routes.module.ts
+++ src/router/routes/admin.routes.module.ts
@@ -3,6 +3,8 @@ import { AdminsModule } from 'src/modules/admins/admins.module';
 import { AdminsController } from 'src/modules/admins/controller/admin.admin-users.controller';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { AuthControllerAdmin } from 'src/modules/auth/controllers/admin.auth.controller';
+import { PostsControllerAdmin } from 'src/modules/posts/controllers/admin.posts.controller';
+import { PostsModule } from 'src/modules/posts/posts.module';
 import { RolesController } from 'src/modules/roles/controllers/admin.roles.controller';
 import { RolesModule } from 'src/modules/roles/roles.module';
 import { TodosControllerAdmin } from 'src/modules/todos/controllers/admin.todos.controller';
@@ -11,13 +13,21 @@ import { UsersControllerAdmin } from 'src/modules/users/controllers/admin.users.
 import { UsersModule } from 'src/modules/users/users.module';
 
 @Module({
-  imports: [AuthModule, AdminsModule, TodosModule, RolesModule, UsersModule],
+  imports: [
+    AuthModule,
+    AdminsModule,
+    TodosModule,
+    RolesModule,
+    UsersModule,
+    PostsModule,
+  ],
   controllers: [
     AuthControllerAdmin,
     AdminsController,
     TodosControllerAdmin,
     RolesController,
     UsersControllerAdmin,
+    PostsControllerAdmin,
   ],
 })
 export class AdminRouteModules {}
--- src/router/routes/public.routes.module.ts
+++ src/router/routes/public.routes.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { AuthControllerPublic } from 'src/modules/auth/controllers/public.auth.controller';
+import { PostsControllerPublic } from 'src/modules/posts/controllers/public-user.posts.controller';
+import { PostsModule } from 'src/modules/posts/posts.module';
 import { TodosControllerPublic } from 'src/modules/todos/controllers/public.todos.controller';
 import { TodosModule } from 'src/modules/todos/todos.module';
 import { UsersControllerPublic } from 'src/modules/users/controllers/public.users.controller';
 import { UsersModule } from 'src/modules/users/users.module';
 
 @Module({
-  imports: [AuthModule, TodosModule, UsersModule],
+  imports: [AuthModule, TodosModule, UsersModule, PostsModule],
   controllers: [
     AuthControllerPublic,
     TodosControllerPublic,
     UsersControllerPublic,
+    PostsControllerPublic,
   ],
 })
 export class PublicRouteModules {}
