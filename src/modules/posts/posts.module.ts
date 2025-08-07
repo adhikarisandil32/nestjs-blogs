896b50f6798b9497e51e538b782f5432420ee22d
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-07 18:02:05 +0545

    fix imports in post.module

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { PostsServiceAdmin } from './services/admin.posts.service';
 import { PostsServicePublic } from './services/public-user.posts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Post } from './entities/post.entity';
+import { Posts } from './entities/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Posts])],
   providers: [PostsServiceAdmin, PostsServicePublic],
   exports: [PostsServiceAdmin, PostsServicePublic],
 })
