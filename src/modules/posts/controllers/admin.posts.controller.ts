4f5e5bf3ffc24e9c96de20717ffcad1a41945f5d
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-07 18:02:49 +0545

    provide required decorators controller logic to admin.posts.controller

--- src/modules/posts/controllers/admin.posts.controller.ts
+++ src/modules/posts/controllers/admin.posts.controller.ts
@@ -6,25 +6,35 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { CreatePostDto } from '../dto/create-post.dto';
 import { PostsServiceAdmin } from '../services/admin.posts.service';
 import { UpdatePostDto } from '../dto/update-post.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { PutAdmin } from 'src/modules/auth/decorator/put-user.decorator';
+import { User } from 'src/common-modules/request/decorators/request.decorator';
+import { Admins } from 'src/modules/admins/entities/admin.entity';
+import { ResponseMessage } from 'src/common-modules/response/decorators/response.decorator';
+import { PaginatedQueryDto } from 'src/common-modules/swagger-docs/paginate-query.dto';
 
 @ApiTags('Posts')
 @Controller('posts')
 export class PostsControllerAdmin {
   constructor(private readonly postsService: PostsServiceAdmin) {}
 
-  @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postsService.create(createPostDto);
+  @ResponseMessage('post create success')
+  @PutAdmin()
+  @Post('create')
+  create(@User() admin: Admins, @Body() createPostDto: CreatePostDto) {
+    return this.postsService.create(admin, createPostDto);
   }
 
+  @ResponseMessage('posts fetch success')
+  @PutAdmin()
   @Get()
-  findAll() {
-    return this.postsService.findAll();
+  findAll(@Query() paginatedQueryDto: PaginatedQueryDto) {
+    return this.postsService.findAll(paginatedQueryDto);
   }
 
   @Get(':id')
