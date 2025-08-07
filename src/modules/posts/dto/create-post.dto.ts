87c6b37d52b8db6a9de7f1ff9c7891abbcd39dad
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-07 18:04:13 +0545

    provide parameters to create-post.dto

--- src/modules/posts/dto/create-post.dto.ts
+++ src/modules/posts/dto/create-post.dto.ts
@@ -1,1 +1,30 @@
-export class CreatePostDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsArray, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Users } from 'src/modules/users/entities/user.entity';
+
+export class CreatePostDto {
+  @ApiProperty({
+    type: 'string',
+    example: 'Post 1',
+  })
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty({
+    type: 'string',
+    example: 'Post Description',
+  })
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @ApiPropertyOptional({
+    type: 'number',
+    example: [1, 2, 3],
+  })
+  @IsArray()
+  @IsNumber({}, { each: true })
+  contributors: number[];
+}
