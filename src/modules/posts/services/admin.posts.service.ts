5167cba53d2fc67cad1a89a9acca6c56ba61e635
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-07 18:04:46 +0545

    create create post service for admin.posts.service

--- src/modules/posts/services/admin.posts.service.ts
+++ src/modules/posts/services/admin.posts.service.ts
@@ -1,15 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePostDto } from '../dto/create-post.dto';
 import { UpdatePostDto } from '../dto/update-post.dto';
+import { Admins } from 'src/modules/admins/entities/admin.entity';
+import { DataSource, Repository } from 'typeorm';
+import { Posts } from '../entities/post.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from 'src/modules/users/entities/user.entity';
+import { PutAdmin } from 'src/modules/auth/decorator/put-user.decorator';
+import { ResponseMessage } from 'src/common-modules/response/decorators/response.decorator';
+import { PaginatedQueryDto } from 'src/common-modules/swagger-docs/paginate-query.dto';
+import { findAllPaginatedData } from 'src/common-modules/utils/functions/common-query';
 
 @Injectable()
 export class PostsServiceAdmin {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(
+    @InjectRepository(Posts)
+    private readonly postsRepository: Repository<Posts>,
+    private readonly _dataSource: DataSource,
+  ) {}
+
+  async create(admin: Admins, createPostDto: CreatePostDto) {
+    const preparedData = await this.prepareCreateData(admin, createPostDto);
+
+    const data = this.postsRepository.create(preparedData);
+
+    await this.postsRepository.save(data);
+
+    return data;
   }
 
-  findAll() {
-    return `This action returns all posts`;
+  async findAll(paginatedQueryDto: PaginatedQueryDto) {
+    const data = await findAllPaginatedData<Posts>({
+      ...paginatedQueryDto,
+      repo: this.postsRepository,
+      validSearchFields: [],
+      validSortFields: [],
+      queryOptions: {
+        select: {
+          // author_admin: {
+          //   id: true,
+          //   email: true,
+          //   name: true,
+          // },
+          author_user: {
+            id: true,
+            email: true,
+            name: true,
+          },
+          contributors: {
+            id: true,
+            email: true,
+            name: true,
+          },
+        },
+        relations: {
+          // author_admin: true,
+          author_user: true,
+          contributors: true,
+        },
+      },
+    });
+
+    return data;
   }
 
   findOne(id: number) {
@@ -22,5 +74,29 @@ export class PostsServiceAdmin {
 
   remove(id: number) {
     return `This action removes a #${id} post`;
+  }
+
+  async prepareCreateData(admin: Admins, createPostDto: CreatePostDto) {
+    try {
+      const contributingUsers = await Promise.all([
+        ...createPostDto.contributors.map((contributor) =>
+          this._dataSource.manager.findOne(Users, {
+            where: { id: contributor },
+          }),
+        ),
+      ]);
+
+      const author = await this._dataSource.manager.findOne(Admins, {
+        where: { id: admin.id },
+      });
+
+      return {
+        ...createPostDto,
+        author_admin: author,
+        contributors: contributingUsers,
+      };
+    } catch (error) {
+      throw new NotFoundException('user not found');
+    }
   }
 }
