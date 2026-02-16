be82622459486d809fc421fc3c28fede27b4627a
Sandil Adhikari <adhikarisandil32@gmail.com>
2026-02-16 17:03:04 +0545

    remove custom logger setup

--- src/common-modules/common.module.ts
+++ src/common-modules/common.module.ts
@@ -1,23 +1,17 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
-import { LoggerModule } from 'nestjs-pino';
 import { ResponseModule } from './response/respose.module';
 import { ErrorFilterModule } from './error/error.module';
-import { CommonUsersModule } from 'src/modules/common-users/common-users.module';
+// import { CommonUsersModule } from 'src/modules/common-users/common-users.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
   imports: [
+    LoggerModule,
     DatabaseModule,
-    LoggerModule.forRoot({
-      pinoHttp: {
-        transport: {
-          target: 'pino-pretty',
-        },
-      },
-    }),
     ResponseModule,
     ErrorFilterModule,
-    CommonUsersModule,
+    // CommonUsersModule,
   ],
 })
 export class CommonModules {}
